ac93453535df44e83b04e38bad8921d6a1c2ce5e
Mark Liu <mark.liu@cba.com.au>
2021-06-04 15:27:58 +1000

    histogram code

--- python/CreateWeatherHistogram/temp_histogram.py
+++ python/CreateWeatherHistogram/temp_histogram.py
@@ -1,15 +1,30 @@
 from collections import Counter
 import numpy as np
+from numpy.core.fromnumeric import size
+from numpy.lib.polynomial import _binary_op_dispatcher
 
-temp_input = (1,2,4,40,12,15,10,22,9,-10,-15,21)
+
+np.random.seed(444)
+np.set_printoptions(precision=3)
+
+d = np.random.laplace(loc=15, scale=3, size=500)
+d[:5]
+hist, bin_edges = np.histogram(d)
+
+a = (1, 2, 4, 40, 12, 15, 10, 22, 9, 5, 30, 21)
 def count_elements(seq) -> dict:
     hist = {}
     for i in seq:
         hist[i] = hist.get(i, 0) + 1
     return hist
 
-counted = count_elements(temp_input)
-recounted = Counter(temp_input)
+bcounts = np.bincount(a)
+hist, _ =np.histogram(a, range=(0, a.max()), bins=a.max() +1)
+dict(zip(np.unique(a), bcounts[bcounts.nonzero()]))
+
+
+counted = count_elements(a)
+recounted = Counter(a)
 
 print (recounted)
 print (counted)
