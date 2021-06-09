b6e53ec2120602bb2d4ae76d5cd0a6a3997ce8d7
Mark Liu <mark.liu@cba.com.au>
2021-06-09 17:07:33 +1000

    working docker

--- python/CreateWeatherHistogram/app/__main__.py
+++ python/CreateWeatherHistogram/app/__main__.py
@@ -1,3 +1,10 @@
+import argparse
 from app.core import main
+from argparse import ArgumentParser
 import sys
-main(sys.argv[1], sys.argv[2], sys.argv[3])
+parser = ArgumentParser()
+parser.add_argument("--inputfile", help="filename of the input file")
+parser.add_argument("--outputfile", help="filename of the output file")
+parser.add_argument("--bucketcount", help="buckercount number", type=int)
+args = parser.parse_args()
+main(inputfile = args.inputfile, outputfile = args.outputfile, bucketcount = args.bucketcount)
--- python/CreateWeatherHistogram/app/core.py
+++ python/CreateWeatherHistogram/app/core.py
@@ -1,9 +1,30 @@
-import numpy as np
-from collections import Counter
-from numpy.core.fromnumeric import size
-from numpy.lib.polynomial import _binary_op_dispatcher
+#import numpy as np
+#from collections import Counter
+#from numpy.core.fromnumeric import size
+#from numpy.lib.polynomial import _binary_op_dispatcher
 
 def main(inputfile, outputfile, bucketcount):
+    
+    dir = "/app/"+outputfile
+
+    print("Output from CreateWeatherHistogram app")
+    infile = open(inputfile,"r")
+    if infile.mode == "r":
+        infile_content = infile.read()
+    print(infile_content)
+    infile.close()
+
+    outputfile_obj = open(dir, "w+")
+    outputfile_obj.write("processed contents from inputfile with a bucketcount of %s" % bucketcount)
+    outputfile_obj.close()
+
+    outfile = open(dir, "r")
+    if outfile.mode == "r":
+        outfile_content = outfile.read()
+        print(outfile_content)
+    outfile.close()
+
+    #https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/sydney?unitGroup=metric&key=5P2L73TBYGGGZKSUTCBFVFKEA
     
     #np.random.seed(444)
     #np.set_printoptions(precision=3)
@@ -23,12 +44,5 @@ def main(inputfile, outputfile, bucketcount):
     #hist, _ =np.histogram(a, range=(0, a.max()), bins=a.max() +1)
     #dict(zip(np.unique(a), bcounts[bcounts.nonzero()]))
 
-
     #counted = count_elements(a)
     #recounted = Counter(a)
-    print ("Output from CreateWeatherHistogram app")
-    print ("using input config for {}".format (inputfile))
-    print ("using output config for {}".format (outputfile))
-    print ("using bucketcount config for {}".format (bucketcount))
-    
-    return outputfile
