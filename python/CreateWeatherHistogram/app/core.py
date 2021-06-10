45d4ce09fcdbdebfe471e227c2c768c885e90e3b
Mark Liu <mark.liu@cba.com.au>
2021-06-10 17:17:22 +1000

    working code

--- python/CreateWeatherHistogram/app/core.py
+++ python/CreateWeatherHistogram/app/core.py
@@ -6,16 +6,13 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import StrMethodFormatter
 
 def main(inputfile, outputfile, bucketcount):
-    
     dir = "/app/"+outputfile
     api_dir = "/app/hist_api.png"
 
     temp_data = pd.read_csv(inputfile, usecols=[17])
-
     temp_data['Temperature'].hist(bins=bucketcount)
     plt.savefig(dir, bbox_inches='tight', dpi=100)
 
-    
     api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?aggregateHours=24&combinationMethod=aggregate&startDateTime=2021-05-01T00%3A00%3A00&endDateTime=2021-06-10T00%3A00%3A00&maxStations=-1&maxDistance=-1&contentType=csv&unitGroup=metric&locationMode=single&key=5P2L73TBYGGGZKSUTCBFVFKEA&dataElements=default&locations=sydney"
     r = rq.get(api_url, verify=False)
     decoded_content = r.content.decode('utf-8')
